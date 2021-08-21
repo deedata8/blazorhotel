7a4804a09e9d46f4aab4f45f92cf9291e69d2e9c
deidra108@gmail.com <deidra108@gmail.com>
2021-08-21 12:26:56 -0600

    first commit

---
+++ HIddenVilla/HIddenVilla_Server/Model/BlazorAmenity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIddenVilla_Server.Model
+{
+    public class BlazorAmenity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
